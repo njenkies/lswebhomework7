b17ac0e24f0c9607efd81740b9592d9092fc8d02
Nathan <njenkins83@yahoo.com>
2017-06-12 22:36:27 -0400

    changes

--- exercises.js
+++ exercises.js
@@ -3,6 +3,14 @@
 function getBiggest(x, y) {
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
+    if (x>y) {
+      return x;
+      }
+      else if (y>x){
+        return y;
+      } else {
+        return x;
+      }
 }
 
 function greeting(language) {
@@ -11,15 +19,34 @@ function greeting(language) {
   //language: 'English' -> 'Hello!'
   //language: 'Spanish' -> 'Hola!'
   //if language is undefined return 'Hello!'
+  if (language === 'German'){
+    return 'Guten Tag!'; 
+  } else if (language === 'English'){
+    return 'Hello!';
+  } else if (language === 'Spanish'){
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   //return true if num is 10 or 5
   //otherwise return false
+  if (num == 10 || num == 5){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   //return true if num is less than 50 and greater than 20
+  if (num < 50 && num > 20){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -29,6 +56,11 @@ function isInteger(num) {
   //-10 -> true
   //otherwise return false
   //hint: you can solve this using Math.floor
+  if (num - Math.floor(num) !=0){
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function fizzBuzz(num) {
@@ -36,6 +68,15 @@ function fizzBuzz(num) {
   //if num is divisible by 5 return 'buzz'
   //if num is divisible by 3 & 5 return 'fizzbuzz'
   //otherwise return num
+  if (num % 3 === 0 && num % 5 === 0){
+    return 'fizzbuzz';
+  } else if (num % 3 === 0){
+    return 'fizz';
+  } else if (num % 5 === 0){
+    return 'buzz';
+  } else {
+    return num;
+  }
 }
 
 function isPrime(num) {
@@ -44,10 +85,20 @@ function isPrime(num) {
   //hint: a prime number is only evenly divisible by itself and 1
   //hint2: you can solve this using a for loop
   //note: 0 and 1 are NOT considered prime numbers
+  if (num < 2){
+    return false;
+  } for (var i = 2; i < num; i++){
+    if (num % i === 0){
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 function returnFirst(arr) {
   //return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
